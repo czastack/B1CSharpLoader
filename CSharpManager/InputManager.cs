0b3c0d9ac9e46d6460020052b4ffd839a37d34fd
czastack <2464851375@qq.com>
2024-09-19 01:47:45 +0800

    fix: IsProgramFocused

--- CSharpManager/InputManager.cs
+++ CSharpManager/InputManager.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Text;
 using CSharpModBase.Input;
 
 namespace CSharpManager
@@ -9,12 +10,28 @@ namespace CSharpManager
         public List<HotKeyItem> BuiltinHotKeyItems { get; } = new();
         public List<HotKeyItem> HotKeyItems { get; } = new();
 
-        private IntPtr HWnd = IntPtr.Zero;
+        private IntPtr HWnd;
 
         public InputManager()
         {
-            var process = Process.GetCurrentProcess();
-            HWnd = process.MainWindowHandle;
+        }
+
+        private void FindMainWindow()
+        {
+            uint currentProcessId = GetCurrentProcessId();
+            StringBuilder stringBuilder = new(64);
+            EnumWindows(new EnumWindowsProc((hWnd, lParam) =>
+            {
+                GetWindowThreadProcessId(hWnd, out uint processId);
+                if (processId == GetCurrentProcessId() &&
+                    GetClassName(hWnd, stringBuilder, stringBuilder.Capacity) > 0 &&
+                    stringBuilder.ToString() == "UnrealWindow")
+                {
+                    HWnd = hWnd;
+                    return false;
+                }
+                return true; // 继续枚举
+            }), IntPtr.Zero);
         }
 
         public void Update()
@@ -63,6 +80,11 @@ namespace CSharpManager
 
         public bool IsProgramFocused()
         {
+            if (HWnd == IntPtr.Zero)
+            {
+                Thread.Sleep(1000);
+                FindMainWindow();
+            }
             var hwnd = GetForegroundWindow();
             if (hwnd == IntPtr.Zero) return false;
             return hwnd == HWnd;
@@ -70,6 +92,21 @@ namespace CSharpManager
 
         [DllImport("user32.dll")]
         private extern static IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetCurrentProcessId();
+
+        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        // 导入Win32 API函数
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
 
         public HotKeyItem RegisterBuiltinKeyBind(Key key, Action action)
         {

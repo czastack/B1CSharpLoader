a050f11f7404b706765f77830d46368dd2845f85
lxymahatma <lxymahatma@gmail.com>
2024-10-19 23:18:53 +0900

    move common dirs into another file

--- CSharpModBase/CSharpModBase.cs
+++ CSharpModBase/CSharpModBase.cs
@@ -1,12 +1,5 @@
 ﻿namespace CSharpModBase;
 
-public static class Common
-{
-    public const string LoaderDir = "CSharpLoader";
-    public const string ModDir = @"CSharpLoader\Mods";
-    public const string DataDir = @"CSharpLoader\Data";
-}
-
 public interface ICSharpMod
 {
     /// <summary>
---
+++ CSharpModBase/CommonDirs.cs
@@ -0,0 +1,8 @@
+namespace CSharpModBase;
+
+public static class CommonDirs
+{
+    public const string LoaderDir = "CSharpLoader";
+    public const string ModDir = @"CSharpLoader\Mods";
+    public const string DataDir = @"CSharpLoader\Data";
+}
